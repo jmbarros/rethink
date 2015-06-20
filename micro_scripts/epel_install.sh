c844284d2ef8ae4ec757a4be8cf5e39bce1b4a0c
Joao Marcelo Barros <joaomarcelobarros@gmail.com>
2015-06-20 12:54:16 -0300

    Micro Scripts

---
+++ micro_scripts/epel_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+####created by jmbarros at tech4it.com.br                              ##### 
+####free to distributing, just give me some credits, okay ? :)         #####
+###########################################################################
+
+yum install epel-release -y
